e6cf422cce7780cd2d6157a4fedd64cfe5077c63
Stephen Albrecht <stpehen.b.albrecht@gmail.com>
2025-09-27 12:29:30 -0400

    javafx hello world

--- src/main/groovy/tabos/Main.groovy
+++ src/main/groovy/tabos/Main.groovy
@@ -1,10 +1,21 @@
 package tabos
 
-class Main {
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.scene.layout.StackPane
+import javafx.stage.Stage
+
+class Main extends Application {
+    @Override
+    void start(Stage stage) {
+        Label label = new Label('Hello world')
+        Scene scene = new Scene(new StackPane(label), 640, 480)
+        stage.setScene(scene)
+        stage.show()
+    }
+
     static void main(String[] args) {
-        InputStream is = new File(args[0]).newInputStream()
-        new GP5InputStream(is).withCloseable {
-            it.readSong()
-        }
+        launch(Main, args)
     }
 }
