6b3fb94f7fc5da3250ba4f2cf7e1c2a8f6e78a26
Stephen Albrecht <stephen.b.albrecht@gmail.com>
2025-10-05 13:50:59 -0400

    log to console tab

---
+++ src/main/groovy/tabos/ui/ConsoleTabController.groovy
@@ -0,0 +1,52 @@
+package tabos.ui
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.Context
+import ch.qos.logback.core.OutputStreamAppender
+import javafx.application.Platform
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.TextArea
+import javafx.scene.text.Font
+import org.slf4j.LoggerFactory
+
+class ConsoleTabController implements Initializable {
+    @FXML private TextArea console
+
+    @Override
+    void initialize(URL url, ResourceBundle resourceBundle) {
+        console.setFont(new Font('Consolas', 12))
+        Logger rootLogger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
+        rootLogger.addAppender(new TextAreaAppender(console).tap {
+            start()
+        })
+    }
+}
+
+class TextAreaAppender extends OutputStreamAppender<ILoggingEvent> {
+    TextAreaAppender(TextArea textArea) {
+        Context lc = LoggerFactory.getILoggerFactory() as Context
+        setContext(lc)
+        encoder = new PatternLayoutEncoder().tap {
+            pattern = "%date %-5level [%thread] %logger{10} %msg%n"
+            setContext lc
+            start()
+        }
+        outputStream = new TextAreaOutputStream(textArea)
+    }
+}
+
+class TextAreaOutputStream extends OutputStream {
+    private final TextArea textArea
+
+    TextAreaOutputStream(TextArea textArea) { this.textArea = textArea }
+
+    @Override void write(int b) throws IOException { super.write(b) }
+
+    @Override
+    void write(byte[] bytes) throws IOException {
+        Platform.runLater(() -> textArea.appendText(new String(bytes)))
+    }
+}
