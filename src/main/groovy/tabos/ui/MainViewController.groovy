cce395631f5a90a324bed2803ff6d099f7e0715c
Stephen Albrecht <stpehen.b.albrecht@gmail.com>
2025-10-06 00:25:21 -0400

    Cleanup

--- src/main/groovy/tabos/ui/MainViewController.groovy
+++ src/main/groovy/tabos/ui/MainViewController.groovy
@@ -14,65 +14,13 @@ class MainViewController implements Initializable {
     @FXML private TabPane editorTabPane
     @FXML private SplitPane verticalSplitPane
     @FXML private SplitPane horizontalSplitPane
-    @FXML private TabPane sidebarTabs
+    @FXML private TabPane leftSidebarTabs
     @FXML private TabPane bottomTabs
-    @FXML private TreeView<String> projectTree
-    @FXML private VBox propertiesBox
-    @FXML private ListView<String> instrumentList
-    @FXML private HBox timeline
-    @FXML private HBox mixer
-    @FXML private VBox scoreArea
 
     @Override
     void initialize(URL location, ResourceBundle resources) {
         horizontalSplitPane.dividers[0].positionProperty().bindBidirectional(BindUtil.prop(config.ui.sidebars, 'left'))
         horizontalSplitPane.dividers[1].positionProperty().bindBidirectional(BindUtil.prop(config.ui.sidebars, 'right'))
         verticalSplitPane.dividers[0].positionProperty().bindBidirectional(BindUtil.prop(config.ui.sidebars, 'bottom'))
-
-        TreeItem<String> root = new TreeItem<>("Project Root")
-        root.expanded = true
-        root.children.addAll(
-            new TreeItem<>("Songs"),
-            new TreeItem<>("Tracks"),
-            new TreeItem<>("Instruments")
-        )
-        projectTree.root = root
-
-        mixer.children.addAll(["Track 1", "Track 2", "Track 3", "Master"].collect { track ->
-            new VBox(5,
-                new Label(track),
-                new Slider(0, 100, 75).tap {
-                    it.orientation = Orientation.VERTICAL
-                    it.prefHeight = 150
-                },
-                new Label("75%")
-            )
-        })
-
-        (1..5).each { trackNum ->
-            VBox staff = new VBox(2)
-            staff.children.add(new Label("Track $trackNum"))
-
-            (0..4).each {
-                Region line = new Region()
-                line.prefHeight = 1
-                line.style = "-fx-background-color: black;"
-                staff.children.add(line)
-            }
-
-            scoreArea.children.add(staff)
-        }
-    }
-
-    @FXML private void handlePlay() {
-        println("Play")
-    }
-
-    @FXML private void handleStop() {
-        println("Stop")
-    }
-
-    @FXML private void handleRecord() {
-        println("Record")
     }
 }
